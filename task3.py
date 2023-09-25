8105c22411a0a30e95fd3bd9d909c1c8311da003
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-09-25 13:22:25 -0700

    yes

--- task3.py
+++ task3.py
@@ -8,3 +8,33 @@ Write a python script display the values of the dictionary
 (3 points)
 """
 sortMe = {1: -2, 2: 6, 4: 0, 6: 1, 9: 2, 10: 3, 11: 0, 13: 3, 14: 4, 15: -2, 17: 0, 18: -1, 20: 3}
+
+
+def sortbykeys():
+  global sortMe
+  myList = []
+  for i in sortMe:
+    myList.append(i)
+  myList.sort()
+  myList2 = []
+  for i in myList:
+    myList2.append(sortMe[i])
+  return myList2
+print(sortbykeys())
+
+def values():
+  global sortMe
+  myList = []
+  for i in sortMe:
+    myList.append(i)
+  myList.sort()
+  myList2 = []
+  for i in myList:
+    myList2.append(sortMe[i])
+    myList2.sort()
+  return myList2
+print(values())
+    
+  
+
+
--- task4.py
+++ task4.py
@@ -1,7 +1,14 @@
 #!python3
 """
 Have the user enter an integer value 'n'
-write a python script to use all of the integers from 1-n as the keys and the squares of those numbers as the values (2 points)
+write a python script to use all of the integers from 1 to n as the keys and the squares of those numbers as the values (2 points)
 sample result:
 x = { 1 : 1, 2 : 4, 3 : 9, 4 : 16, 5 : 25, 6 : 36, 7 : 49, 8 : 64, 9 : 81, 10 : 100 }
 """
+
+n = int(input("enter in a value:  "))
+vocab = {}
+for i in range(n+1):
+    number = i ** 2
+    vocab[i] = number
+print(vocab)
