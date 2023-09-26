6c066016cefe548e754f2b81e458a38dfecbc95d
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-09-26 13:27:25 -0700

    yes

--- task5.py
+++ task5.py
@@ -1,7 +1,10 @@
 #!python3
 """
 ##### Task 5
-create a dictionary for an inventory of items in a game.  Ask the user for input, and if the item they choose to 'get item', add that item to their inventory.  If they choose to drop item' remove that item from that invenory.  If they choose 'show inventory' give them a list of the items that they have.
+create a dictionary for an inventory of items in a game.  
+Ask the user for input, and if the item they choose to 'get item', add that item to their inventory. 
+ If they choose to drop item' remove that item from that invenory.  
+ If they choose 'show inventory' give them a list of the items that they have.
 
 Possible extensions:
 * nicer format for displaying inventory
@@ -36,3 +39,50 @@ You have:
  3 wood
  >
 """
+stuff = {"food" : 0 ,"water" : 0 ,"rope" : 0 ,"torch" : 0 , "sack" : 0 ,
+ "wood" : 0 , "iron" : 0 , "steel" : 0 , "ginger" : 0 , "garlic" : 0 ,
+"fish" : 0 , "stone" : 0 , "wool" : 0}
+
+while True:
+    select = input("Do you want to drop item or get item or check inventory: ")
+    select.lower()
+    bag = 0
+    
+    if select == "inventory":
+        for i in stuff:
+            if stuff[i] == 0:
+                pass
+            else:
+                print(stuff[i], {i} )
+                bag =+ 1
+
+        if bag == 0:
+            print("you ain't got shit")
+
+
+    if select == "get" or select == "get item":
+        item = input("what do you want: ")
+        amount = int(input("how many do you want: "))
+        for i in stuff:
+            stuff[ item ] = amount
+            print(stuff[item])
+
+
+        
+        
+            
+            
+            
+        '''
+    for i in stuff:
+        if select == "get" or "get item":
+            item = input("What item do you want: ")
+            amount = input("how much do you want: ")
+
+'''       
+
+            
+
+
+
+
---
+++ test.py
@@ -0,0 +1,10 @@
+stuff = {"food" : 0 ,"water" : 0 ,"rope" : 0 ,"torch" : 0 , "sack" : 0 ,
+ "wood" : 0 , "iron" : 0 , "steel" : 0 , "ginger" : 0 , "garlic" : 0 ,
+"fish" : 0 , "stone" : 0 , "wool" : 0}
+
+
+stuff['food'] = 8
+print(stuff['food'])
+
+stuff['food'] += 3
+print(stuff['food'])
